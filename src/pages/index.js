babffdc493a6846954538b848027aaf36cf67802
Sergey Skrebnev <sergey.skrebnev@gmail.com>
2020-12-29 19:34:20 +0300

    new cali high remix

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,6 +105,7 @@ class RootIndex extends React.Component {
             <div>
               <a className="contact-icon facebook" target="_blank" href="https://www.facebook.com/mtrss.art"></a>
               <a className="contact-icon instagram" target="_blank" href="https://www.instagram.com/mtrss.art"></a>
+              <a className="contact-icon spotify" target="_blank" href="https://open.spotify.com/artist/6k4nRFSKe2EQuzMHAtY1gp?si=05TzF1roQUqSdPnhDWxWBA"></a>
               <a className="contact-icon bandcamp" target="_blank" href="https://mtrss.bandcamp.com/"></a>
               <a className="contact-icon email" target="_blank" href="mailto:mtrss.art@gmail.com"></a>
             </div>
