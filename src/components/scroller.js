fab076313f2dd4a13055b9ff386dc8fc8fed6c82
Sergey Skrebnev <sergey.skrebnev@gmail.com>
2021-03-09 18:48:23 +0300

    +video

--- src/components/scroller.js
+++ src/components/scroller.js
@@ -51,11 +51,12 @@ const Scroller = () => {
         dotListClass="custom-dot-list-style"
         itemClass="carousel-item-padding-40-px"
       >
-        <YoutubeSlide key="youtube-1" url="https://www.youtube.com/embed/ZLSMMfBei18" />
-        <YoutubeSlide key="youtube-2" url="https://www.youtube.com/embed/bClwEwAdfMM" />
-        <YoutubeSlide key="youtube-3" url="https://www.youtube.com/embed/-hxCYIFnCYU" />
-        <YoutubeSlide key="youtube-4" url="https://www.youtube.com/embed/dGJ-GD7M9lA" />
-        <YoutubeSlide key="youtube-5" url="https://www.youtube.com/embed/ZLSMMfBei18" />
+        <YoutubeSlide key="youtube-1" url="https://www.youtube.com/embed/SZzd_Vt_eXQ" />
+        <YoutubeSlide key="youtube-2" url="https://www.youtube.com/embed/ZLSMMfBei18" />
+        <YoutubeSlide key="youtube-3" url="https://www.youtube.com/embed/bClwEwAdfMM" />
+        <YoutubeSlide key="youtube-4" url="https://www.youtube.com/embed/-hxCYIFnCYU" />
+        <YoutubeSlide key="youtube-5" url="https://www.youtube.com/embed/dGJ-GD7M9lA" />
+        <YoutubeSlide key="youtube-6" url="https://www.youtube.com/embed/ZLSMMfBei18" />
       </Carousel>
     </>
   )
